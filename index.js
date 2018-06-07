c8ebcd0999f3b6631c3bdde3d6e2182fdae6bda4
Shaun Cutts <shaunc@factfiber.com>
2018-06-07 11:32:56 -0400

    fix load for non-coffee environments

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 // require coffee if possible; js otherwise
 try {
-  try { require('coffee-script/register'); } catch (e) {}  
+  require('coffeescript/register');
   postgres = require('./src/postgres');
 }
 catch (e) {
-  if(e.message.indexOf("Cannot find module") != -1 
-      && (e.message.indexOf('./src/index') != -1 
-        || e.message.indexOf('coffee-script/register') != -1))
-    postgres = require('./lib/postgres');
-  else
-    throw e;
+  postgres = require('./lib/postgres');
 }
 module.exports = postgres;
